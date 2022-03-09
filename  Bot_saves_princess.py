6746876a2639e328b151ad77e546378d06418b9c
kutayeroglu <amarauteerasmus@gmail.com>
2022-03-09 20:54:41 +0300

    Solution added

---
+++  Bot_saves_princess.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+def displayPathtoPrincess(n,grid):
+#print all the moves here
+    # Bot is in the middle
+    m = int((n-1)/2)
+    
+    # 4 possible locations for the princess
+    if grid[0][0] == "p": 
+        for i in range(m):
+            print("UP")
+            print("LEFT")
+    elif grid[0][n-1] == "p":
+        for i in range(m):
+            print("UP")
+            print("RIGHT")
+    elif grid[n-1][0] == "p":
+        for i in range(m):
+            print("DOWN")
+            print("LEFT")
+    elif grid[n-1][n-1] == "p":
+        for i in range(m):
+            print("DOWN")
+            print("RIGHT")
+    else:
+        return
+
+m = int(input())
+grid = [] 
+for i in range(0, m): 
+    grid.append(input().strip())
+
+displayPathtoPrincess(m,grid)
